6491c2a80a7827db7e08ecbf12150cdfb4bf345d
deepikanara79 <deepikanara79@gmail.com>
2026-06-25 22:26:52 +0530

    list program

---
+++ list.py
@@ -0,0 +1,25 @@
+age=[10,23,13,14,56]
+print(age)
+#accessing through index
+print(age[3])
+print(age[-4])
+#change through index
+age[4]=40
+print(age)
+#adding element
+age.append(60)
+print(age)
+#insert function
+age.insert(2,30)
+print(age)
+#remove function
+age.remove(10)
+print(age)
+#pop function
+age.pop(3)
+print(age)
+#slice function
+print(age[2:4])
+#length of list
+print(len(age))
+
